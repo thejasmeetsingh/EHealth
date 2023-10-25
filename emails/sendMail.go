5688dd0b63cbc52fb7ec42497700c5b5fa530926
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2023-10-25 13:46:26 +0530

    Added reset password templates and send mail function

---
+++ emails/sendMail.go
@@ -0,0 +1,43 @@
+package emails
+
+import (
+	"os"
+	"strconv"
+
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/gomail.v2"
+)
+
+func Send(recipients []string, subject string, body string) {
+	message := gomail.NewMessage()
+
+	fromEmail := os.Getenv("FROM_EMAIL")
+	smtpServer := os.Getenv("SMTP_SERVER")
+	smtpPortStr := os.Getenv("SMTP_PORT")
+	smtpUsername := os.Getenv("SMTP_USERNAME")
+	smtpPassword := os.Getenv("SMTP_PASSWORD")
+
+	if fromEmail == "" || smtpServer == "" || smtpPortStr == "" || smtpUsername == "" || smtpPassword == "" {
+		log.Errorln("Cannot send email. Email credentials are not connfigured in the env")
+		return
+	}
+
+	message.SetHeader("From", fromEmail)
+	message.SetHeader("To", recipients...)
+	message.SetHeader("Subject", subject)
+	message.SetBody("text/html", body)
+
+	smtpPort, err := strconv.Atoi(smtpPortStr)
+	if err != nil {
+		smtpPort = 587
+	}
+
+	dialer := gomail.NewDialer(smtpServer, smtpPort, smtpUsername, smtpPassword)
+
+	if err := dialer.DialAndSend(message); err != nil {
+		log.Errorln("Caught error while sending email: ", err)
+		return
+	}
+
+	log.Infoln("Email sent successfully!")
+}
