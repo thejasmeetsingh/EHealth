08587836e8a97c11407b7f9f468d54b46bbfedea
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2023-10-26 13:04:07 +0530

    Refactor the auth middleware

--- middlewares/auth.go
+++ middlewares/auth.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/thejasmeetsingh/EHealth/handlers"
 	"github.com/thejasmeetsingh/EHealth/internal/database"
 	"github.com/thejasmeetsingh/EHealth/utils"
@@ -31,14 +32,21 @@ func JWTAuth(apiCfg handlers.ApiCfg, handler authHandler) gin.HandlerFunc {
 			return
 		}
 
-		claims, err := utils.VerifyAccessToken(authToken[1])
+		claims, err := utils.VerifyToken(authToken[1])
 		if err != nil {
 			handlers.ErrorResponse(ctx, http.StatusForbidden, fmt.Sprintf("Error caught while verifying the token: %v", err))
 			ctx.Abort()
 			return
 		}
 
-		dbUser, err := apiCfg.DB.GetUserById(ctx, claims.UserID)
+		userID, err := uuid.Parse(claims.Data)
+		if err != nil {
+			handlers.ErrorResponse(ctx, http.StatusForbidden, "Invalid authentication token")
+			ctx.Abort()
+			return
+		}
+
+		dbUser, err := apiCfg.DB.GetUserById(ctx, userID)
 		if err != nil {
 			handlers.ErrorResponse(ctx, http.StatusForbidden, "Invalid authentication token")
 			ctx.Abort()
