f93e6d596c71fd44bf6778e38e47419bdd8309df
Jasmeet Singh <thejasmeet.singh88629@gmail.com>
2023-10-29 22:58:00 +0530

    Updated to test case

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -12,7 +12,7 @@ import (
 func TestSignUpAPI(t *testing.T) {
 	router := config.GetRouter(true)
 
-	payload := []byte(`{"email": "testing-user@example", "password": "12345678Aa@", "is_end_user": true}`)
+	payload := []byte(`{"email": "testing-user@example.com", "password": "12345678Aa@", "is_end_user": true}`)
 
 	req := httptest.NewRequest(http.MethodPost, "/v1/signup/", bytes.NewBuffer(payload))
 	req.Header.Set("Content-Type", "application/json")
